b7c00cecb1a4b506dcfaee2ab482d8bce880c0ec
bondilirithika <168219157+bondilirithika@users.noreply.github.com>
2024-09-11 00:58:47 +0530

    comments added

--- 1.Print_1_to_n.java
+++ 1.Print_1_to_n.java
@@ -8,9 +8,14 @@ class Solution{
       }
     static void print1_n(int n){
       if(n==0){
+          //base case
         return;
         }
-      print1_n(n-1);
-      System.out.print(n+" ");
+      print1_n(n-1);//this will go until the base case is reached
+      System.out.print(n+" ");//continues its execution after the base case has reached
     }
   }
+
+//explanation:
+//n=5->n!=0->calls func->n=4,not 0,calls func->n=3,not 0,calls func=>n=2,not 0,calls func=>n=1 not 0 calls func=>n=0 so it return to n=1 and remaining statement is executed so prints 1 then return to n=2 then prints 2
+//then returns to n=3 and prints 3 then n=4 the n=5
