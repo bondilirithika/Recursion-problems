e2c19291e5e8714dcb30145c86b733c37fc293b8
bondilirithika <168219157+bondilirithika@users.noreply.github.com>
2024-09-11 11:58:39 +0530

    complexities added

--- 1.Print_1_to_n.java
+++ 1.Print_1_to_n.java
@@ -19,3 +19,6 @@ class Solution{
 //explanation:
 //n=5->n!=0->calls func->n=4,not 0,calls func->n=3,not 0,calls func=>n=2,not 0,calls func=>n=1 not 0 calls func=>n=0 so it return to n=1 and remaining statement is executed so prints 1 then return to n=2 then prints 2
 //then returns to n=3 and prints 3 then n=4 the n=5
+
+TIME COMPLEXITY:the function runs for about n+1 times so,time complexity->O(n)
+SPACE COMPLEXITY:We are only declaring some variables of constant size,space complexity->O(1) but as we are using recursion,due to recursion stack the space complexity->O(n)
