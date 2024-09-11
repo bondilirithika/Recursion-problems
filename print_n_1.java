98c269e0754c9bef5fd28dcec5fc9495fe961950
bondilirithika <168219157+bondilirithika@users.noreply.github.com>
2024-09-11 11:53:57 +0530

    Create print_n_1.java

---
+++ print_n_1.java
@@ -0,0 +1,18 @@
+//Print n to 1 
+import java.util.Scanner;
+  class Solution{
+  public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter n:");
+    int n=sc.nextInt();
+    print_n_1(n);
+  }
+  static void print_n_1(int n)
+  {
+    if(n==0)//base case
+      return;
+    System.out.print(n+" ");
+    //we are printing the number first and then going to n-1 this will continue till the number reaches the last desired output ie.,1 if it goes any further ie., to 0 then we return
+    print_n_1(n-1);
+  }
+}
